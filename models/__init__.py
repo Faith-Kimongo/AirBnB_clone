bc069454038a63a2c9d30b761c3e9b07406f0a9d
root <root@DESKTOP-SIEPB5L>
2024-03-12 10:22:18 +0200

    init code

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+import Filestorage module
+"""
+from models.engine.file_storage import FileStorage
+
+"""
+create the variable storage, an instance of FileStorage
+"""
+storage = FileStorage()
+storage.reload()
